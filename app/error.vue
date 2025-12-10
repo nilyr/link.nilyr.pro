2c4f76d9e8c85fddde44ef19ac05ccadfb813343
Nilyr <me@nilyr.ru>
2025-12-11 02:13:12 +0300

    Update error.vue

--- app/error.vue
+++ app/error.vue
@@ -1,5 +1,18 @@
-<script setup>
+<script setup lang="ts">
+import type { NuxtError } from '#app'
 import errorImage from './assets/images/404.svg?raw'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+
+const route = useRoute()
+
+if (props.error?.statusCode === 404) {
+  if (!route.path.startsWith('/dashboard')) {
+    clearError({ redirect: 'https://nilyr.pro' })
+  }
+}
 </script>
 
 <template>
